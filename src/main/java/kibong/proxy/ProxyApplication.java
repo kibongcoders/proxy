91a6982c9d499f8d30654fb782e452f912e33bdc
kibongcoders <kkp02052@gmail.com>
2023-08-14 21:10:21 +0900

    V1 : 스프링 빈으로 수동 등록

--- src/main/java/kibong/proxy/ProxyApplication.java
+++ src/main/java/kibong/proxy/ProxyApplication.java
@@ -1,9 +1,12 @@
 package kibong.proxy;
 
+import kibong.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(AppV1Config.class) //하지 않으면 자동 컴포넌트 스캔 대상이 됨
+@SpringBootApplication(scanBasePackages = "kibong.proxy.app")
 public class ProxyApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/kibong/proxy/proxy.md
@@ -0,0 +1,9 @@
+<h1>프록시</h1>
+<ul>
+<h2>V1 : 스프링 빈으로 수동 등록</h2>
+<li>
+<p>
+    SpringMVC는 @Controller 또는 @ReqeustMapping을 포함하고 있어야 인식한다.
+</p>
+</li>
+</ul>
